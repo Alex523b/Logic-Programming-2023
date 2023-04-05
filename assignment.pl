7fe9a69cf99d6d49580e7b0d8877ee5d9bda39f5
Alexandros Ntiveris <alexhles957@gmail.com>
2023-04-06 02:32:05 +0300

    assignment with duplicate solutions; to-do: remove duplicates

--- assignment.pl
+++ assignment.pl
@@ -1,66 +1,74 @@
 % Αλέξανδρος Ντιβέρης - 1115201900136
-activity(1, act(0,3)). activity(2, act(0,4)). activity(3, act(1,5)).
-activity(4, act(4,6)). activity(5, act(6,8)). activity(6, act(6,9)).
-activity(7, act(9,10)). activity(8, act(9,13)). activity(9, act(11,14)).
 
-assignment(NPersons, Assignment, ASP) :-
-	findall(Aid, activity(Aid, _), AIds), % gatherSoFarActivities all activities in list AIds
-    computeInitialASP(NPersons, ASP),
-	assign(AIds, NPersons, Assignment, ASP).
+% REMOVE THEM BEFORE SUMBITTING
+ activity(a1, act(0,3)).
+ activity(a2, act(4,6)).
+ activity(a3, act(1,2)).
 
-assign([], _, [], _).
-assign([AId|AIds], NPersons, [AId-PId|Assignment], ASP) :-
-	between(1, NPersons, PId), % Select a person PId for activity AId
-    updateElement(PId, AId, ASP, NewASP),
-    assign(AIds, NPersons, Assignment, NewASP),
-	gatherSoFarActivities(PId, Assignment, APIds), % Gather in list APIds so far activities of PId
-	activity(AId, act(Ab, Ae)),
-	valid(Ab, Ae, APIds). % Is current assignment consistent with previous ones?
+assignment(NP, MT, ASP, ASA) :-
+    findall(Aid, activity(Aid, _), AIds), % gather_so_far_activities all activities in list AIds
+    assign(AIds, NP, MT, ASA),
+    compute_initial_ASP(NP, InitialASP),
+    compute_final_ASP(ASA, InitialASP, ASP).
 
-valid(_, _, []).
-valid(Ab1, Ae1, [APId|APIds]) :-
-	activity(APId, act(Ab2, Ae2)),
-	Ab2 > Ae1, % fill
-	valid(Ab1, Ae1, APIds).
+compute_final_ASP([], L, L).
+compute_final_ASP([AId - PId|Rest], InitialASP, ASP) :-
+    update_element(PId, AId, InitialASP, NewASP),
+    compute_final_ASP(Rest, NewASP, ASP).
 
-between(L, U, L) :-
-	L =< U.
-between(L, U, X) :-
-	L < U,
-	L1 is L + 1,
-	between(L1, U, X).
+compute_initial_ASP(NP, InitializedASP) :-
+    all_between(1, NP, InitialASP),
+    fill_initial_ASP(InitialASP, InitializedASP).
 
-all_between(L, U, [L|X]) :-
-	L =< U,
-	L1 is L + 1,
-	all_between(L1, U, X).
-all_between(L, U, []) :-
-	L > U.
+fill_initial_ASP([], []).
+fill_initial_ASP([PId|L], [PId - [] - 0|T]) :-
+    fill_initial_ASP(L, T).
 
-gatherSoFarActivities(_, [], []).
-gatherSoFarActivities(PId, [_-PId2|L], T) :-
+assign([], _, _, []).
+assign([AId|AIds], NP, MT, [AId-PId|Assignment]) :-
+    assign(AIds, NP, MT, Assignment),
+    between(1, NP, PId), % Select a person PId for activity AId
+    activity(AId, act(Ab, Ae)),
+    gather_so_far_activities(PId, Assignment, APIds), % Gather in list APIds so far activities of PId
+    valid(Ab, Ae, APIds, 0, MT). % Is current assignment consistent with previous ones?
+
+valid(As1, Ae1, [], Sum, MT) :-
+    Sum + Ae1 - As1 =< MT.
+valid(As1, Ae1, [APId|APIds], Sum, MT) :-
+    activity(APId, act(As2, Ae2)),
+    (As2 > Ae1 ; As1 > Ae2),
+    UpdatedSum is Sum + Ae2 - As2,
+    valid(As1, Ae1, APIds, UpdatedSum, MT).
+
+gather_so_far_activities(_, [], []).
+gather_so_far_activities(PId, [_-PId2|L], T) :-
     PId =\= PId2,
-    gatherSoFarActivities(PId, L, T).
-gatherSoFarActivities(PId, [AId-PId|L], [AId|T]) :-
-    gatherSoFarActivities(PId, L, T).
+    gather_so_far_activities(PId, L, T).
+gather_so_far_activities(PId, [AId-PId|L], [AId|T]) :-
+    gather_so_far_activities(PId, L, T).
 
-computeInitialASP(NPersons, InitializedASP) :-
-    all_between(1, NPersons, InitialASP),
-    fillInitialASP(InitialASP, InitializedASP).
-
-fillInitialASP([], []).
-fillInitialASP([PId|L], [PId - [] - 0 | T]) :-
-    fillInitialASP(L, T).
-
-updateElement(PId, AId, L, UpdatedL) :-
-    updateElement(PId, AId, L, [], TempUpdatedL),
+update_element(PId, AId, L, UpdatedL) :-
+    update_element(PId, AId, L, [], TempUpdatedL),
     reverse(TempUpdatedL, UpdatedL).
-updateElement(_, _, [], L, L).
-updateElement(PId, AId, [PId - X - Y | T], SoFarL, UpdatedL) :-
-    append(X, [AId], X2),
+update_element(_, _, [], L, L).
+update_element(PId, AId, [PId - X - Y|T], SoFarL, UpdatedL) :-
     activity(AId, act(B, E)),
     NewDuration is Y + E - B,
-    updateElement(PId, AId, T, [PId - X2 - NewDuration | SoFarL], UpdatedL).
-updateElement(PId, AId, [PId2 - X - Y | T], SoFarL, UpdatedL) :-
+    update_element(PId, AId, T, [PId - [AId|X] - NewDuration|SoFarL], UpdatedL).
+update_element(PId, AId, [PId2 - X - Y|T], SoFarL, UpdatedL) :-
     PId =\= PId2,
-    updateElement(PId, AId, T, [PId2 - X - Y | SoFarL], UpdatedL).
+    update_element(PId, AId, T, [PId2 - X - Y|SoFarL], UpdatedL).
+
+between(L, U, L) :-
+    L =< U.
+between(L, U, X) :-
+    L < U,
+    L1 is L + 1,
+    between(L1, U, X).
+
+all_between(L, U, [L|X]) :-
+    L =< U,
+    L1 is L + 1,
+    all_between(L1, U, X).
+all_between(L, U, []) :-
+    L > U.
