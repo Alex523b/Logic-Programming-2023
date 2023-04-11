c7d5e9234fb8e23ca6873b8ae8fb364da49b7ab1
Alexandros Ntiveris <alexhles957@gmail.com>
2023-04-11 22:57:53 +0300

    replaced ic library with fd for performance reasons

--- numpart.pl
+++ numpart.pl
@@ -1,30 +1,31 @@
 % Αλέξανδρος Ντιβέρης - 1115201900136
 
-:- lib(ic).
+:- lib(fd).
 
 numpart(N, L1, L2) :-
-    N mod 2 =:= 0,
+    length(L3, N),
+    L3 :: 1..N,
+    alldifferent(L3),
+
     N_half is N // 2,
     length(L1, N_half),
     length(L2, N_half),
-    L1 #:: 1..N,
-    alldifferent(L1),
-    alldifferent(L2),
-    L2 #:: 1..N,
     append(L1, L2, L3),
-    alldifferent(L3),
     sum(L1) #= sum(L2),
+
     compute_squares(L1, L1_squared),
     compute_squares(L2, L2_squared),
     sum(L1_squared) #= sum(L2_squared),
 
+    % eliminate permutations in each list
     is_sorted(L1),
     is_sorted(L2),
-    append([X], _, L1),
-    append([Y], _, L2),
-    X #< Y,
-    search([L1, L2], 0, first_fail, indomain, complete, []).
 
+    % eliminate permutation of L1 and L2
+    append([X], _, L1),
+    X #= 1,
+
+    generate_most_constrained(L3).
 
 compute_squares([],[]).
 compute_squares([H|L], [H * H|L1]) :-
@@ -35,3 +36,9 @@ is_sorted([_]).
 is_sorted([X,Y|L]) :-
     X #=< Y,
     is_sorted([Y|L]).
+
+generate_most_constrained([]).
+generate_most_constrained(Columns) :-
+   deleteffc(Column, Columns, RestColumns),
+   indomain(Column),
+   generate_most_constrained(RestColumns).
