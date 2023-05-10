68e13409fb709570e40b156a5379a3b7294631fa
Alexandros Ntiveris <alexhles957@gmail.com>
2023-05-10 22:27:26 +0300

    improved performance

--- numpart.pl
+++ numpart.pl
@@ -1,41 +1,31 @@
 % Αλέξανδρος Ντιβέρης - 1115201900136
 
-:- lib(fd).
+:- lib(gfd).
 
 numpart(N, L1, L2) :-
-    variables(N, L1, L2, L3),
-    constrain(L1, L2, L3),
-    generate_most_constrained(L3).
+    N mod 2 =:= 0, % ensure that N is even
+    variables(N, L1),
 
-variables(N, L1, L2, L3) :-
-    length(L3, N),
-    L3 :: 1..N,
-    N_half is N // 2,
-    length(L1, N_half),
-    length(L2, N_half),
-    append(L1, L2, L3).
-
-constrain(L1, L2, L3) :-
-    alldifferent(L3),
-    sum(L1) #= sum(L2),
+    % constraints
+    alldifferent(L1),
+    is_sorted(L1), % symmetry breaking constraint
+    sum(L1) #= (N*(N+1)) / 4,
 
     compute_squares(L1, L1_squared),
-    compute_squares(L2, L2_squared),
-    sum(L1_squared) #= sum(L2_squared),
+    sum(L1_squared) #= (N*(N + 1)*(2*N + 1)) / 12,
 
-    % eliminate permutations in each list
-    is_sorted(L1),
-    is_sorted(L2),
-
-    % eliminate permutation of L1 and L2
     append([X], _, L1),
-    X #= 1.
+    X #= 1, % symmetry breaking constraint
 
-generate_most_constrained([]).
-generate_most_constrained(Columns) :-
-   deleteffc(Column, Columns, RestColumns),
-   indomain(Column),
-   generate_most_constrained(RestColumns).
+    search(L1, 0, most_constrained, indomain, complete, []),
+
+    all_between(1, N, L3),
+    set_difference(L3, L1, L2).
+
+variables(N, L1) :-
+    N_half is N // 2,
+    length(L1, N_half), % there is no need to compute both lists; L2 will be extracted from {1, 2, ..., N-1, N} - L1
+    L1 #:: 1..N.
 
 % auxiliary predicates
 compute_squares([],[]).
@@ -47,3 +37,18 @@ is_sorted([_]).
 is_sorted([X,Y|L]) :-
     X #=< Y,
     is_sorted([Y|L]).
+
+all_between(L, U, [L|X]):-
+    L =< U,
+    L1 is L + 1,
+    all_between(L1, U, X).
+all_between(L, U, []):-
+    L > U.
+
+set_difference([], _, []).
+set_difference([H|T], L, [H|L1]):-
+    \+member(H, L),
+    set_difference(T, L, L1).
+set_difference([H|T], L, L1):-
+    member(H, L),
+    set_difference(T, L, L1).
